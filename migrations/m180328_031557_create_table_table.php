facc07a40d9ab7eefc11a505198fa66600fc99c8
Nick Tsai <myintaer@gmail.com>
2018-06-09 01:55:44 +0800

    Add defaultValue for migration

--- migrations/m180328_031557_create_table_table.php
+++ migrations/m180328_031557_create_table_table.php
@@ -14,7 +14,7 @@ class m180328_031557_create_table_table extends Migration
     {
         $this->createTable('records', [
             'id' => $this->primaryKey(),
-            'title' => $this->string()->notNull(),
+            'title' => $this->string()->notNull()->defaultValue(''),
             'updated_at' => $this->dateTime(),
             'created_at' => $this->dateTime(),
         ]);
